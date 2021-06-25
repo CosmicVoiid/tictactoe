667cc93e0d07d4a60b4a0b0b2953d29832d938e3
Saurav Sah <sah_64@yahoo.com>
2021-06-25 11:03:59 -0400

    Add display controller

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const GameBoard = (() => {
-	let gameBoard = ["O", "O", "X", "O", "X", "X", "O", "X", "X", "O"];
-
-	return { gameBoard };
+	const grid = document.querySelector("#game-board");
+	let gameBoard = ["O", "O", "X", "O", "X", "X", "O", "X", "X"];
+	const displayController = () => {
+		for (let i = 0; i < gameBoard.length; i++) {
+			const gridItem = document.createElement("div");
+			gridItem.classList.add("game-marker");
+			gridItem.textContent = gameBoard[i];
+			grid.appendChild(gridItem);
+		}
+	};
+	return { gameBoard, displayController };
 })();
 
 const playerFactory = (player, mark) => {
@@ -9,5 +17,7 @@ const playerFactory = (player, mark) => {
 };
 
 const player1 = playerFactory("player1", "X");
+const player2 = playerFactory("player2", "O");
 
 console.log(player1.mark);
+GameBoard.displayController();
